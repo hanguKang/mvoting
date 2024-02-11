05dccb45451cb84917c426d0a878d2dd09e790da
redhangu <redhangu@gmail.com>
2024-02-11 21:37:16 +0900

    javascript파일 common파일 작성

--- src/js/main.js
+++ src/js/main.js
@@ -22,6 +22,7 @@ import bootstrapMin from "bootstrap/dist/js/bootstrap.min";
 // document.body.appendChild(component());
 
 $(function () {
+  MVOTING_LAYOUT.init();
   //carousel
   const carousel = new bootstrapMin.Carousel("#myCarousel");
   //modal
@@ -52,3 +53,54 @@ const doNav = () => {
   const nav1 = "#nav1";
   new Nav(nav1);
 };
+
+const MVOTING_LAYOUT = {
+  is_mobile: false,
+  inint() {
+    let window_width = $(window).width();
+    this.mobile_check(window_width);
+  },
+  mobile_check(window_width) {
+    if (window_width < 721) {
+      this.is_mobile = true;
+    } else {
+      this.is_mobile = false;
+    }
+    //this.main_grid();
+  },
+};
+// jQuery(".Page .main_menu>nav>ul").mouseleave(function () {
+//   let no_delay = true;
+//   //let menu_detail = null;
+//   //KEAD_LAYOUT.header_enter = true;
+//   KEAD_LAYOUT.mouse_prev = true;
+//   KEAD_LAYOUT.hover_link = $(this).children("a");
+//   KEAD_LAYOUT.menu_detail_prev = $(KEAD_LAYOUT.hover_link)
+//     .closest("li")
+//     .index();
+//   //alert(KEAD_LAYOUT.menu_detail_prev);
+//   KEAD_LAYOUT.close_menu_detail(false, no_delay);
+// });
+// jQuery(".Page .main_menu>nav>ul").mouseleave(function () {
+//   KEAD_LAYOUT.close_menu_detail();
+// });
+// jQuery(".Page .main_menu>nav>ul>li").mouseenter(function () {
+//   //console.log('123456');
+//   let delay = true;
+//   KEAD_LAYOUT.hover_link = $(this).children("a");
+//   KEAD_LAYOUT.menu_detail_idx = $(KEAD_LAYOUT.hover_link).parent("li").index();
+//   //alert(KEAD_LAYOUT.menu_detail_idx);
+//   if (KEAD_LAYOUT.mouse_prev) {
+//     KEAD_LAYOUT.header_enter = false;
+//   } else {
+//     KEAD_LAYOUT.header_enter = true;
+//   }
+
+//   if (KEAD_LAYOUT.header_enter) {
+//     delay = true;
+//   } else {
+//     delay = false;
+//   }
+
+//   KEAD_LAYOUT.open_menu_detail(KEAD_LAYOUT.hover_link, delay);
+// });
